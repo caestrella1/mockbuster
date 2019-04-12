e2281ec573a0cb75c31f155ced1b0f83db8c6403
Torin Foss <tfoss@csumb.edu>
2019-04-12 05:40:47 +0000

    started getAllItems but its not working, maybe a cloud 9 issue

---
+++ api/getAllItems.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * getAllItems.php | 2019 | Torin, David, Carlos, Q
+ * 
+ * Returns all the movies with itemTable.
+ */
+include '../backend/dbConnection.php';
+$conn = getDatabaseConnection("movie");
+
+$sql = "SELECT * FROM itemTable WHERE 1";
+
+// print($sql);
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$resp = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($resp);
+
+?>
