ea182d898c0822432eab913c48c5f520f35b8a4d
Torin Foss <tfoss@csumb.edu>
2019-04-12 05:00:18 +0000

    Updated populateItemDB.php

--- backend/populateItemDB.php
+++ backend/populateItemDB.php
@@ -1,65 +1,40 @@
 <?php
-// session_start(); //starts or resumes an existing session
+/**
+ * populateItemDB.php | 2019 | Torin, David, Carlos, Q
+ * 
+ * Used to populate our itemTable in our MySQL database.
+ */
 
 include 'dbConnection.php';
-
 $conn = getDatabaseConnection("movie");
-
 
 $filename = "top20.json";
 $data = file_get_contents($filename);
-
 $array = json_decode($data, true);
-// print_r($array["results"][0]);
-
 
 for($i = 0; $i < 1; $i++){
     $obj = $array["results"][$i];
-    // print_r($obj);
+
+    $name = $obj['title'];
+    $desc = $obj['overview'];
+    $poster = $obj['poster_path'];
+    $backdrop = $obj['backdrop_path'];
+    $rating = $obj['vote_average'];
     $rand = mt_rand(10, 20);
     $rand = $rand + .99;
-    // $namedParameters = array();
-    // $nameParamters
-     $sql = "INSERT INTO `itemTable` (`name`, `description`, `poster`, `backdrop`, `rating`, `price`) VALUES ('".$obj['title'] . "', '" .$obj['overview'] . "', '" .$obj['poster_path'] . "', '" .$obj['backdrop_path'] . "', '" .$obj['vote_average'] . "', '" . $rand . "');";
-    //$sql = "INSERT into itemTable ('name', 'description', 'poster', 'backdrop', 'rating', 'price') VALUES ('".$obj['title'] . "', '" .$obj['overview'] . "', '" .$obj['poster_path'] . "', '" .$obj['backdrop_path'] . "', '" .$obj['vote_average'] . "', '" . $rand . "');";
-    // $sql = "INSERT into itemTable ('name', 'description', 'poster', 'backdrop', 'rating', 'price') VALUES ( '$obj["title]', '$obj['overview']', '" .$obj['poster_path'] . "', '" .$obj['backdrop_path'] . "', '" .$obj['vote_average'] . "', " . $rand . ");";
-    
+
+     $sql = "INSERT INTO itemTable (name, description, poster, backdrop, rating, price) VALUES ";
+     $sql .= "('$name', '$desc', '$poster', '$backdrop', '$rating', '$rand');";
     print($sql);
-    // $stmt = $conn->prepare($sql);
-    // $stmt->execute();
+    try {
+        // $stmt = $conn->prepare($sql);
+        // $stmt->execute();
+    } catch (Exception $e){
+        // not the best way, but I think some of the paths have a forward slash
+        // in them which is messing with our query, so I'm not sure how to deal
+        // that, so our table will just be missing a few movies...
+        continue;
+    }
     
 }
-
-
-// $sql = "SELECT * FROM users WHERE username = :username AND password = :password";
-
-// $namedParameters = array();
-// $namedParameters[':username'] = $username;
-// $namedParameters[':password'] = $password;
-
-
-// $stmt = $conn->prepare($sql);
-// $stmt->execute($namedParameters);
-// $record = $stmt->fetch(PDO::FETCH_ASSOC); //we are expecting ONLY one record, so we use fetch instead of fetchAll
-
-// // print_r($record);
- 
-//  if (empty($record)) {
-     
-//      echo "Username or Password are incorrect!";
-     
-     
-//  }  else {
- 
-//     //echo $record[0]['firstName']; //using fetchAll
-//     //echo $record['firstName'] . " " . $record['lastName'] ; //using fetch
-    
-//     $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
-//     // header('location: admin.php'); //redirecting to a new file
-//     echo "successful login";
-    
-    
-
-// }
-
 ?>
