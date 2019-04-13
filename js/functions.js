4bbe62a8e04ee3d9613202061e95674839030a46
Carlos Estrada <cestrrada@gmail.com>
2019-04-13 22:02:53 +0000

    Added movie ratings, added function to check cart size and update its color accordingly

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,18 @@
 /**** Functions for manipulating the UI ****/
 /* global $ */
 
-function addMoviePoster(id, imgEndpoint) {
+function addMoviePoster(id, name, imgEndpoint, rating) {
     let image = "https://image.tmdb.org/t/p/w500/" + imgEndpoint;
+    rating = parseFloat(rating) / 2.0;
     $("#top-movies").append(
         `<div class="col-12 col-lg-3 mb-4">` +
-            `<a href="movie?id=${id}"><div class="movie shadow">` +
-                `<img src="${image}" class="card-img rounded-lg shadow" alt="">` +
-            `</div></a>` +
+            `<div class="movie">` +
+                `<div class="movie-poster-container rounded-lg">` +
+                    `<img src="${image}" class="movie-poster card-img" alt="${name} poster">` +
+                `</div>` +
+                `<h5 class="movie-title text-dark mt-2 mb-0">${name}</h5>` +
+                `<i class="fas fa-star mr-1 text-warning"></i><span class="text-dark">${rating}</span>` +
+            `</div>` +
         `</div>`
     );
 }
@@ -22,4 +27,25 @@ function admin_newMovie(url="", img="") {
             `<button class="btn btn-outline-danger update-movie">Delete</button>` +
         `</div>`
     );
+}
+
+/* Updates the color of the cart according to whether or not it's empty */
+function updateCartColor() {
+    let cartCount = parseInt($("#cart-count").html());
+    
+    if (cartCount > 0) {
+        $("#cart").removeClass("btn-outline-light");
+        $("#cart").addClass("btn-info");
+    }
+    else {
+        $("#cart").removeClass("btn-info");
+        $("#cart").addClass("btn-outline-light");
+    }
+}
+
+/* DEBUGGING: test function for updateCartColor */
+function incrementCart() {
+    let cartCount = parseInt($("#cart-count").html());
+    $("#cart-count").html(++cartCount);
+    updateCartColor();
 }
