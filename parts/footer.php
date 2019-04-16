f0745a6251ccf5385b55f2f1e7a5483ab495eb53
Carlos Estrada <cestrrada@gmail.com>
2019-04-16 07:01:15 +0000

    Credit TMDb for movie info

--- parts/footer.php
+++ parts/footer.php
@@ -1,10 +1,14 @@
 <!-- Footer used on every page -->
 <footer class="<?=$site["theme-bg"]?> text-center <?=$site["theme-text"]?> shadow">
-    <div class="container py-2">
-        <span class="d-block">&copy; 2019 <?=$site["title"]?></span>
-        <span class="d-block">
+    <div class="container pt-2 pb-3">
+        <p class="m-2">&copy; 2019 <?=$site["title"]?></p>
+        <p class="m-2">
             Disclaimer: The information on this webpage is fictitious.
             It is used for academic purposes only.
-        </span>
+        </p>
+        <p class="m-2">
+            Movie posters and descriptions provided by 
+            <a href="https://www.themoviedb.org/" target="_blank" class="text-info">The Movie Database (TMDb)</a>.
+        </p>
     </div>
 </footer>
