2d3d6e2c74309031cccc2ca4ddb1e2eb08200a84
Carlos Estrada <cestrrada@gmail.com>
2019-04-17 00:54:19 +0000

    Start cart.php

---
+++ cart.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include "parts/head.php" ?>
+        <title>Cart | <?=$site["title"]?></title>
+        <script>
+            $(function() {
+                
+            });
+        </script>
+    </head>
+    <body>
+        <?php include "parts/nav.php" ?>
+        
+        <main class="container">
+            <div class="row">
+                <div class="col-12"></div>
+            </div>
+        </main>
+        
+        <?php include "parts/footer.php" ?>
+    </body>
+</html>
