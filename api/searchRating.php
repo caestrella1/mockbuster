5c4f53594ee958d240d9f5935c0bf77688703dee
espiritu324 <46942365+espiritu324@users.noreply.github.com>
2019-04-22 13:40:08 -0700

    creation of searchRating.php

---
+++ api/searchRating.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * searchRating.php | 2019 | Torin, David, Carlos, Q
+ * 
+ * Gets int(1-5) selected by user
+ * Searches for movies with rating specified and up
+ * returns all items that match
+ * 
+ * 
+ */
+include '../backend/dbConnection.php';
+$conn = getDatabaseConnection("movie");
+
+$rating = $_POST['rating'];
+$sql = "SELECT * FROM itemTable WHERE rating>$rating;";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$resp = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($resp);
+?>
