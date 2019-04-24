d29057153945fd6fbac3a3ba14a28e17444522ba
espiritu324 <46942365+espiritu324@users.noreply.github.com>
2019-04-23 17:48:40 -0700

    Create filterAll.php

---
+++ api/filterAll.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * searchPhrase.php | 2019 | Torin, David, Carlos, Q
+ * 
+ * Gets Phrase entered by user
+ * Searches through DB and returns whatever items matches search
+ * 
+ */
+include '../backend/dbConnection.php';
+$conn = getDatabaseConnection("movie");
+
+//Hardcode for testing
+// $phrase = "captain";
+// $rating = 3;
+// $price = 0;
+
+$phrase = $_POST['phrase'];
+$rating = $_POST['rating'] * 2;
+$price = $_POST['price'];
+
+if($price=0){
+    $order="ASC";
+}else{
+    $order="DESC";
+}
+
+if($phrase==""){
+    $sql = "SELECT * FROM itemTable WHERE rating>$rating ORDER BY price $order;";
+}else{
+$sql = "SELECT * FROM itemTable WHERE 
+(name LIKE '%$phrase%' OR description LIKE '%$phrase%') AND (rating>$rating) ORDER BY price $order;";
+}
+
+//runs statement
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$resp = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($resp);
+?>
